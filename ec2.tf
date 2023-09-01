e398e2892ee6a7ce328ee37b5e37d3f75185ccc1
simisola88 <simisaheyi@gmail.com>
2023-09-01 02:06:40 +0100

    Updated Remote provisioner Sec-Group for internet access

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,47 @@
 provider "aws" {
-  region     = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instancetype
+  key_name      = var.keyname
 
-   provisioner "local-exec" {
-    command = "echo ${aws_instance.myec2.private_ip} >> private_ips.txt"
+  connection {
+    type        = var.connectiontype
+    user        = var.user
+    private_key = file("./terraform-key.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # Updating with the latest command for Amazon Linux machine
+      "sudo yum install -y nginx",
+      "sudo systemctl start nginx"
+    ]
+  }
+}
+
+resource "aws_security_group" "allow_multiple_ports" {
+  name        = "allow port multiple ports"
+  description = "Allow inbound traffic"
+
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = [var.vpn_ip]
+    }
+  }
+  egress {
+    description = "Outbound rules"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
   }
 }
--- remoteProvisioner/ec2.tf
+++ remoteProvisioner/ec2.tf
@@ -1,26 +1,26 @@
 provider "aws" {
-  region     = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "myec2" {
-   ami = var.ami_id
-   instance_type = var.instancetype
-   key_name = var.keyname
+  ami           = var.ami_id
+  instance_type = var.instancetype
+  key_name      = var.keyname
 
-   connection {
-   type     = var.connectiontype
-   user     = var.user
-   private_key = file("./terraform-key.pem")
-   host     = self.public_ip
-    }
+  connection {
+    type        = var.connectiontype
+    user        = var.user
+    private_key = file("./terraform-key.pem")
+    host        = self.public_ip
+  }
 
- provisioner "remote-exec" {
-   inline = [
-    # Updating with the latest command for Amazon Linux machine
-     "sudo yum install -y nginx",
-     "sudo systemctl start nginx"
-   ]
- }
+  provisioner "remote-exec" {
+    inline = [
+      # Updating with the latest command for Amazon Linux machine
+      "sudo yum install -y nginx",
+      "sudo systemctl start nginx"
+    ]
+  }
 }
 
 resource "aws_security_group" "allow_multiple_ports" {
@@ -31,10 +31,17 @@ resource "aws_security_group" "allow_multiple_ports" {
     for_each = var.sg_ports
     iterator = port
     content {
-        from_port        = port.value
-        to_port          = port.value
-        protocol         = "tcp"
-        cidr_blocks      = [var.vpn_ip]
-        }
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = [var.vpn_ip]
     }
+  }
+  egress {
+    description = "Outbound rules"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
+  }
 }
